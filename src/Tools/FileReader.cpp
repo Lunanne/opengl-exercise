460e2f917d1e0a81098d4d754c0c61d6a11aa39d
Anne Marije v/d Meer <anne.marije.meer@gmail.com>
2014-04-30 14:46:21 +0200

    Fixing things

--- src/Tools/FileReader.cpp
+++ src/Tools/FileReader.cpp
@@ -27,7 +27,7 @@ char* FileReader::ReadFile(const char* p_fileName)
         file.seekg(0, std::ios::beg);
         file.read(content, length);
         content[length] = '\0';
-        
+
         return content;
     }
     else
@@ -40,50 +40,58 @@ char* FileReader::ReadFile(const char* p_fileName)
 Image FileReader::ReadPNG(const char* p_fileName)
 {
     Image image;
-    
+
     png_structp imagePtr;
     png_infop infoPtr;
-    png_bytep* rowPtr;
-    
+
     int bitDepth, colorType;
     png_uint_32 height, width;
     FILE* file = fopen(p_fileName, "rb");
     imagePtr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
     infoPtr = png_create_info_struct(imagePtr);
-    
+
     if (setjmp(png_jmpbuf(imagePtr)))
         std::printf("Error during png stuff");
-    
+
     png_init_io(imagePtr, file);
-    png_read_png(imagePtr, infoPtr, PNG_TRANSFORM_IDENTITY, NULL);
+    png_read_info(imagePtr, infoPtr);
     png_get_IHDR(imagePtr, infoPtr, &width, &height, &bitDepth, &colorType, NULL, NULL, NULL);
-    
+
+    if (bitDepth != 8)
+        std::printf("Error, unsupported bit depth");
+
     image.width = width;
     image.height = height;
     image.bitDepth = bitDepth;
     switch (colorType) {
-        case PNG_COLOR_TYPE_RGB:
-            image.format = GL_RGB;
-            break;
-        case PNG_COLOR_TYPE_RGB_ALPHA:
-            image.format = GL_RGBA;
+    case PNG_COLOR_TYPE_RGB:
+        image.format = GL_RGB;
+        break;
+    case PNG_COLOR_TYPE_RGB_ALPHA:
+        image.format = GL_RGBA;
+        break;
+    default:
+        std::printf("Error unsupported format %d \n", colorType);
+        break;
     }
-    
+
+    png_read_update_info(imagePtr, infoPtr);
+
+
     int rowbytes = png_get_rowbytes(imagePtr, infoPtr);
-    rowPtr = png_get_rows(imagePtr, infoPtr);
-    size_t size = image.width * image.height * png_get_channels(imagePtr, infoPtr);
+    rowbytes += 3 - ((rowbytes - 1) % 4);
+    size_t size = rowbytes * image.height * sizeof(png_byte*) +15;
     image.data = static_cast<unsigned char*>(malloc(size));
-    int pixelCount = 0;
-    for(int row = height - 1; row >= 0; --row)
+    png_byte** rowPtr = static_cast<png_byte**>(malloc(image.height * sizeof(png_byte *)));
+    //point rowPtr to correct offset in image data
+    for (int i = 0; i < image.height; ++i)
     {
-        for(int x = 0; x < rowbytes; ++x)
-        {
-            image.data[pixelCount] = rowPtr[row][x];
-            ++pixelCount;
-        }
+        rowPtr[image.height - 1 - i] = image.data + i * rowbytes;
     }
+    png_set_interlace_handling(imagePtr);
+    png_read_image(imagePtr, rowPtr);
     fclose(file);
     png_destroy_read_struct(&imagePtr, &infoPtr, NULL);
-    
+    free(rowPtr);
     return image;
 }
